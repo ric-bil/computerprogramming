9f804dbc94b4b23f12282664d03fb5cbb5bfeb83
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-03-11 23:09:07 +0100

    Update task01.py

--- lab02/task01.py
+++ lab02/task01.py
@@ -26,4 +26,4 @@ weatherData = json.loads(response.text)
 #print(weatherData['main']['temp_max']) 
 
 st.header("Weather forecast")
-st.text(weatherData['main']['temp_max'])
+st.text("Temperature in Bolzano: " + weatherData['main']['temp_max'] + " °C")

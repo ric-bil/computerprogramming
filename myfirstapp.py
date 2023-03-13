3dadec23c68fb80055fa5cf4fb3fffc4d53e60ab
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-03-13 16:29:42 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,3 +1,3 @@
 import streamlit as st
-title = st.text_input('Please insert a movie title', '')
+title = st.text_input('Please insert a movie title', 'Write here')
 st.write('The current movie title is', title)

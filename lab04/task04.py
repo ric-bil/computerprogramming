614d422b201f592a486a6fd5e876705c1fe46afb
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-03-25 13:08:40 +0100

    Update task04.py

--- lab04/task04.py
+++ lab04/task04.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 #add your own APIkey
 option = st.selectbox('Please, select an option', ('Synonyms', 'Antonyms', 'Sounds like', 'Means like')) 
-keyword = st.input('Insert a keyword')
+keyword = st.text_input('Insert a keyword')
 
 if option:
     if option=='Synonyms':

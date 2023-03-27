caf6f7532df3941359f06a1e1c8d7e51e04d768f
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-03-27 09:48:13 +0200

    Update task04.py

--- lab05/task04.py
+++ lab05/task04.py
@@ -3,10 +3,10 @@ import json, requests
 from googletrans import Translator
 import streamlit as st
 
-option = st.selectbox('Please, select an destionation language', ('it', 'de', 'en', 'fr')) 
+language = st.selectbox('Please, select an destionation language', ('it', 'de', 'en', 'fr')) 
 keyword = st.text_input('Insert a word or a sentence')
 
 
-if (option and keyword):
+if (language and keyword):
     translate = translator.translate(word, dest=language) 
     print(translate)

2c0310dce9ae665f8a5dbb29ff6bb3a0509d1130
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-03-27 11:08:21 +0200

    Update task04.py

--- lab05/task04.py
+++ lab05/task04.py
@@ -5,8 +5,8 @@ import streamlit as st
 
 language = st.selectbox('Please, select an destionation language', ('it', 'de', 'en', 'fr')) 
 keyword = st.text_input('Insert a word or a sentence')
-
+translator = Translator()
 
 if (language and keyword):
-    translate = translator.translate(keyword, dest='it') 
+    translate = translator.translate(keyword, dest=language) 
 

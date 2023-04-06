d1061c108d5edf4012941d4b22587b5af3da7b49
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-04-07 00:38:05 +0200

    Create task03.py

---
+++ lab06/task03.py
@@ -0,0 +1,16 @@
+#! python3
+from gtts import gTTS
+from googletrans import Translator
+import IPython.display as ipd  
+import streamlit as st
+
+
+translator = Translator()
+word = st.input("Please, insert a word/sentence\t")
+lang_code = st.input("Please, type a 2-letter language code, for the destination language ")
+translate = translator.translate(word, dest=lang_code) 
+tts1=gTTS(text=translate.text, lang=lang_code)
+tts1.save('file.mp3')
+
+audio_file = open("file.mp3", "rb")
+st.audio(data=audio_file, format="audio/mp3", start_time=0)

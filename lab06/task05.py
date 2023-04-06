cb82276eb8ec49849854e481a18e31d5cbbec4fd
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-04-07 01:25:45 +0200

    Update task05.py

--- lab06/task05.py
+++ lab06/task05.py
@@ -8,12 +8,11 @@ lang_code = st.text_input("Please, type a 2-letter language code, for the destin
 uploaded_file = st.file_uploader("Choose a file", type=["txt"])
 
 if uploaded_file is not None:
-  st.write(uploaded_file.getvalue().decode("utf-8"))
-  #translate = translator.translate(uploaded_file.getvalue(), dest=lang_code) 
-  #tts1=gTTS(text=translate.text, lang=lang_code)
-  #tts1.save('file.mp3')
+  translate = translator.translate(uploaded_file.getvalue().decode("utf-8"), dest=lang_code) 
+  tts1=gTTS(text=translate.text, lang=lang_code)
+  tts1.save('file.mp3')
 
-  #audio_file = open("file.mp3", "rb")
-  #st.audio(data=audio_file, format="audio/mp3", start_time=0)
+  audio_file = open("file.mp3", "rb")
+  st.audio(data=audio_file, format="audio/mp3", start_time=0)
   
-  #st.download_button(label="Download", data=audio_file, file_name='file.mp3',mime='audio/mp3')
+  st.download_button(label="Download", data=audio_file, file_name='file.mp3',mime='audio/mp3')

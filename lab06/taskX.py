6ee4a8ca4f412927ea9f5470bbdb65f82a0e890a
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-04-11 16:15:52 +0200

    Update taskX.py

--- lab06/taskX.py
+++ lab06/taskX.py
@@ -3,6 +3,7 @@ from gtts import gTTS
 from googletrans import Translator
 import streamlit as st
 import time
+import base64
 
 translator = Translator()
 lang_code = st.text_input("Please, type a 2-letter language code, for the destination language")
@@ -18,13 +19,36 @@ if uploaded_file is not None:
   
 
 
-  html_string = """
-            <audio controls autoplay>
-              <source src="./file.mp3" type="audio/mp3">
+  #html_string = """
+  #          <audio controls autoplay>
+  #            <source src="./file.mp3" type="audio/mp3">
+  #          </audio>
+  #          """
+
+  #sound = st.empty()
+  #sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
+  #time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
+  #sound.empty()  # optionally delete the element afterwards
+  #st.write("# Auto-playing Audio!")
+
+  autoplay_audio("file.mp3")
+  
+
+
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
             </audio>
             """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
-  sound = st.empty()
-  sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
-  time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
-  #sound.empty()  # optionally delete the element afterwards
+
+

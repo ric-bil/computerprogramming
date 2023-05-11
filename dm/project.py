54c8a98e31521ce1f2f028fa0cbf4456a49c66a0
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-05-11 19:34:44 +0200

    Update project.py

--- dm/project.py
+++ dm/project.py
@@ -5,14 +5,15 @@ import streamlit as st
 items = ['to_eat', 'to_read', 'tree', 'apple']
 rand_item = random.choice(items)
 
-st.button(label='START')
-st.write(rand_item)
+clicked = st.button(label='START')
+if clicked:
+  st.write(rand_item)
   #picture = "images/" + rand_item + '.jpg'
   #img = Image.open(picture)
   #st.image(img, width=300)
   
-user_input = st.text_input("Enter the word")
+  user_input = st.text_input("Enter the word")
 
-if user_input:
-  st.write(user_input)
-  st.button(label='PLAY AGAIN')
+  if user_input:
+    st.write(user_input)
+    st.button(label='PLAY AGAIN')

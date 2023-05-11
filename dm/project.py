6ddb82ab2d60056cd3e05e5e5aae835f4a13a36f
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-05-11 19:43:53 +0200

    Update project.py

--- dm/project.py
+++ dm/project.py
@@ -5,22 +5,25 @@ import streamlit as st
 items = ['to_eat', 'to_read', 'tree', 'apple']
 rand_item = random.choice(items)
 
-st.button(label='START')
-st.write('IMMAGINE ' + rand_item)
-#picture = "images/" + rand_item + '.jpg'
-#img = Image.open(picture)
-#st.image(img, width=300)
+st.button(label='START', on_click=playGame)
+
+def playGame():
+
+  st.write('IMMAGINE ' + rand_item)
+  #picture = "images/" + rand_item + '.jpg'
+  #img = Image.open(picture)
+  #st.image(img, width=300)
   
-user_input = st.text_input("Enter the word")
+  user_input = st.text_input("Enter the word")
 
-if user_input:	
-  st.write(user_input)
+  if user_input:	
+    st.write(user_input)
 
-  if user_input.lower() == str(rand_item):
-    st.write("You entered the correct word!")
-  else:
-    st.write("Incorrect word. Please try again or get a hint.")
-    st.write("\n")
-    st.write("\n")
-    #    option = st.selectbox("Choose one for help", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
-    #st.button(label='PLAY AGAIN')
+    if user_input.lower() == str(rand_item):
+      st.write("You entered the correct word!")
+    else:
+      st.write("Incorrect word. Please try again or get a hint.")
+      st.write("\n")
+      st.write("\n")
+      #    option = st.selectbox("Choose one for help", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
+      #st.button(label='PLAY AGAIN')

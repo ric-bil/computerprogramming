28745771764020a4a333d5b1116dc3fefb55c0cf
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-05-11 19:15:56 +0200

    Update project.py

--- dm/project.py
+++ dm/project.py
@@ -6,7 +6,7 @@ items = ['to_eat', 'to_read', 'tree', 'apple']
 rand_item = random.choice(items)
 
 if st.button(label='START'):
-  st.write("IMAGE:" + rand_item)
+  st.write(rand_item)
   #picture = "images/" + rand_item + '.jpg'
   #img = Image.open(picture)
   #st.image(img, width=300)

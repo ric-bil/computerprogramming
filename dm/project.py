132a63f0717b5cd8f22c5ed696fb7b7cb82a3e24
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-05-11 19:11:10 +0200

    Update project.py

--- dm/project.py
+++ dm/project.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 items = ['to_eat', 'to_read', 'tree', 'apple']
 rand_item = random.choice(items)
+st.write(rand_item)
 
 if st.button(label='START'):
   picture = "images/" + rand_item + '.jpg'

bee038d09c43827f5eb447169c6c5e4412f6ee0e
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-05-11 19:39:20 +0200

    Update project.py

--- dm/project.py
+++ dm/project.py
@@ -16,12 +16,11 @@ user_input = st.text_input("Enter the word")
 if user_input:	
   st.write(user_input)
 
-	if user_input.lower() == str(rand_item):
-	  st.write("You entered the correct word!")
-	else:
-	  st.write("Incorrect word. Please try again or get a hint.")
-	  st.write("\n")
-	  st.write("\n")
-	#    option = st.selectbox("Choose one for help", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
-	
-  #st.button(label='PLAY AGAIN')
+  if user_input.lower() == str(rand_item):
+    st.write("You entered the correct word!")
+  else:
+    st.write("Incorrect word. Please try again or get a hint.")
+    st.write("\n")
+    st.write("\n")
+    #    option = st.selectbox("Choose one for help", ["None selected. Select your hint", "It is another word for", "It sounds like", "Similar in meaning to", "It rhymes with"])
+    #st.button(label='PLAY AGAIN')

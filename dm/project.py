6dd4bf35389aaeed998a67e1521f60867250c806
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-05-11 19:16:53 +0200

    Update project.py

--- dm/project.py
+++ dm/project.py
@@ -10,3 +10,8 @@ if st.button(label='START'):
   #picture = "images/" + rand_item + '.jpg'
   #img = Image.open(picture)
   #st.image(img, width=300)
+  
+  user_input = st.text_input("Enter the word")
+  
+  if user_input:
+    st.write(user_input)

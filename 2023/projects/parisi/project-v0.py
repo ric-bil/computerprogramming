39e6934984536f6e9efc2f8beab36d9f5c03ee8d
ric-bil <127631182+ric-bil@users.noreply.github.com>
2023-06-08 15:38:37 +0200

    Create project-v0.py

---
+++ 2023/projects/parisi/project-v0.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import stanza
+from googletrans import Translator
+translator = Translator()
+
+if 'clicked' not in st.session_state:
+  st.session_state['clicked'] = '0'
+else:
+  clicked = st.session_state.clicked
+
+st.title('Super Translaton 3000 (-1)')
+st.header('Welcome to the slowest, somewhat buggy but functioning text analyser ever!')
+st.write('''This app allows you to translate and then analyse a text in any language,
+as long as Stanza and Google Translate support it. To use it, just type a text in any language of your
+choice. It will be automatically recognized. Then, choose a language. You can write the
+name of the language in English or just type the two code-letter (e.g. for Spanish, you
+can either type "Spanish" or "es"). You will have a translation and then you can
+walk through the sentences of the translated text. When selecting a sentence,
+you will have clickable single tokens. When clicking them, you will get the lemma, the part
+of speech and the grammatical features (as in the text) of the desired word. \n
+Friendly reminder: *Good things come to those who wait.*''')
+text = None
+input_text = st.text_area('Please, insert text here')
+dest_lang = st.text_input('Enter a language here')
+st.subheader("Translation")
+if (input_text and dest_lang):
+ with st.spinner("Translating text..."):
+  try:
+     output_dict = translator.translate(input_text, dest=dest_lang)
+     translated_text = (output_dict.text)
+     st.write(translated_text)
+  except ValueError:
+     st.warning(f"{dest_lang} is not a valid language!")
+     text = None
+     dest_lang = None
+else:
+    st.info ("Oops! Something is missing!")
+
+st.subheader("Analyzer")
+if (input_text and dest_lang):
+  with st.spinner("Analysing text..."):
+   try:
+     stanza.download(dest_lang)
+     lan_nlp = stanza.Pipeline(f"{dest_lang}", processors = "tokenize, mwt, lemma, pos" )
+     text = lan_nlp(translated_text)
+   except stanza.pipeline.core.UnsupportedProcessorError:
+     st.info ("Sorry, text in this language can not be analyzed.")
+     text = None
+   except stanza.resources.common.UnknownLanguageError:
+      st.warning("This code is unknown! Try typing the language name in full charachters.")
+
+upos_dict = {
+    'ADJ': 'Adjective',
+    'ADP': 'Adposition',
+    'ADV': 'Adverb',
+    'AUX': 'Auxiliary verb',
+    'CCONJ': 'Coordinating conjunction',
+    'DET': 'Determiner',
+    'INTJ': 'Interjection',
+    'NOUN': 'Noun',
+    'NUM': 'Numeral',
+    'PART': 'Particle',
+    'PRON': 'Pronoun',
+    'PROPN': 'Proper noun',
+    'PUNCT': 'Punctuation',
+    'SCONJ': 'Subordinating conjunction',
+    'SYM': 'Symbol',
+    'VERB': 'Verb',
+    'X': 'Other'
+}
+duplicate_avoider = 0
+if text:
+ for i, sent in enumerate(text.sentences):
+  sentence_text = sent.text
+  if st.button(f"Sentence {i+1}: {sentence_text}", key=f"sentence_{i+1}"):
+     st.session_state['clicked'] = i+1
+  if st.session_state['clicked'] == i+1:
+     st.write(f"Sentence {i+1}:")
+     for x, word in enumerate(sent.words):
+      if word.pos == 'PUNCT':
+       continue
+      duplicate_avoider += 1
+      word_text = str(word.text)
+      if st.button(word_text, key=f"word_{duplicate_avoider}"):
+       lemma = word.lemma
+       upos = word.upos
+       feats = word.feats
+       upos_label = upos_dict.get(upos, 'Unknown')
+       st.info(f"**Lemma**: {lemma}; **Part of Speech**: {upos_label}, **Features**: {feats}")
+      else:
+       pass
+else:
+ pass
